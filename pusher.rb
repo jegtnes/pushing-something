3d0485446ff9c7b8412ec95af6bc29c25368cc1f
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-02-17 20:05:44 +0000

    Submitting stuff with AJAX works now

--- pusher.rb
+++ pusher.rb
@@ -10,7 +10,7 @@ get '/' do
   erb :index, :layout => :layout
 end
 
-post '/' do
+post '/message' do
   Pusher.trigger('test_channel', 'message', {message: params['message']})
-  params['message']
+  erb :index, :layout => :layout
 end
