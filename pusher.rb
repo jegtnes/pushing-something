10a04c2b0d1bfa8cab50d4524629e429d37fa733
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-02-17 19:49:42 +0000

    Configuring Pusher stuff

--- pusher.rb
+++ pusher.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
+require 'pusher'
 
+Pusher.app_id = '107544'
+Pusher.key = '3c5b5ffb49fe35b8d7bd'
+Pusher.secret = 'a27b0a0996119dcdf528'
 
 
 get '/' do
   erb :index, :layout => :layout
+end
+
+post '/' do
+
 end
